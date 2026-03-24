46f283acccabd0143471b2df1484621b973093cf
Om Satapathy <omsatapathy7@gmail.com>
2026-03-24 20:34:13 +0530

    array prom

--- src/main/java/day9/CommonElements.java
+++ src/main/java/day9/CommonElements.java
@@ -22,8 +22,24 @@ public class CommonElements {
 				duplicate.add(i);
 			}
 		}
-		
+
 		System.out.println(duplicate);
+
+		// fibonacci series
+
+		// 0 1 1 2 3 5 8 13
+
+		int n = 10, a = 0, b = 1;
+		System.out.println("Fibonacci Series till " + n + " terms:");
+
+		for (int i = 1; i <= n; ++i) {
+			System.out.print(a + ", ");
+
+			// compute the next term
+			int nextTerm = a + b;
+			a = b;
+			b = nextTerm;
+		}
 	}
 
 }
