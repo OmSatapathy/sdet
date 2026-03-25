230625b8020d10da2ece1436d14898626a30d1d9
Om Satapathy <omsatapathy7@gmail.com>
2026-03-25 13:01:03 +0530

    string prog

---
+++ src/main/java/day9/Removefirstlast.java
@@ -0,0 +1,13 @@
+package day9;
+
+public class Removefirstlast {
+
+	public static void main(String[] args) {
+
+		String str = "automation testing";
+		
+		String str1 = str.substring(1, str.length()-1);
+		System.out.println(str1);
+	}
+
+}
