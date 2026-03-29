080986afed40b53c116c8e439414fb0d1f4c9eb8
Om Satapathy <omsatapathy7@gmail.com>
2026-03-29 22:03:29 +0530

    iterator code

---
+++ src/main/java/day10/Iterator.java
@@ -0,0 +1,28 @@
+package day10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Iterator {
+
+	public static void main(String[] args) {
+
+		List<String> lst = new ArrayList<>();
+		lst.add("ram");
+		lst.add("sam");
+		lst.add("dinesh");
+		lst.add("malay");
+
+		java.util.Iterator<String> itr = lst.iterator();
+		while (itr.hasNext()) {
+			String string = (String) itr.next();
+			System.out.println(string);
+		//	lst.add("prakash"); will create cuncorrentmodification exception
+
+		}
+		
+		
+	}
+
+}
