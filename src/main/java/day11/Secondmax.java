73a08ffaa5ccd9ceb76a5575a0f1ec11a57666ab
Om Satapathy <omsatapathy7@gmail.com>
2026-04-05 15:31:57 +0530

    code addeding

---
+++ src/main/java/day11/Secondmax.java
@@ -0,0 +1,23 @@
+package day11;
+
+public class Secondmax {
+
+	public static void main(String[] args) {
+		int[] arr = {33,55,22,556,64,344,123};
+		int max=arr[0];
+		int secmax=arr[0];
+		
+		 for (int i = 0; i < arr.length; i++) {
+	            if (arr[i] > max) {
+	                secmax = max;   // previous max becomes second max
+	                max = arr[i];
+	            } else if (arr[i] > secmax && arr[i] != max) {
+	                secmax = arr[i];
+	            }
+	        }
+
+		
+		System.out.println(secmax);
+	}
+
+}
