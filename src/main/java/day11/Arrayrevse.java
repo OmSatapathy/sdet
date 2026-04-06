0a41a5e9c0e8e7e28dd0756ace592b362bbefcac
Om Satapathy <omsatapathy7@gmail.com>
2026-04-06 22:56:52 +0530

    java prog pratices

--- src/main/java/day11/Arrayrevse.java
+++ src/main/java/day11/Arrayrevse.java
@@ -3,24 +3,37 @@ package day11;
 public class Arrayrevse {
 
 	public static void main(String[] args) {
-		int[] arr = {43,54,66,3,7,11,556};
-		int start= 0;
-		int end = arr.length-1;
-		
-		if(start<end) {
+		int[] arr = { 43, 54, 66, 3, 7, 11, 556 };
+		int start = 0;
+		int end = arr.length - 1;
+
+		if (start < end) {
 			int temp = arr[start];
-			arr[start]= arr[end];
-			arr[end]= temp;
-			
+			arr[start] = arr[end];
+			arr[end] = temp;
+
 			start++;
 			end--;
-			
+
+		}
+
+		for (int p : arr) {
+			//System.out.println(p);
+
+		}
+
+		int max = arr[0];
+		int min = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			} else if (arr[i] < min) {
+				min = arr[i];
+			}
 		}
 		
-		for(int p: arr) {
-			System.out.println(p);
-			
-		}
+		System.out.println(min);
 	}
 
 }
